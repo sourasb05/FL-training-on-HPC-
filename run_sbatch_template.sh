f42993e19be4251d0707798735c40cb8f274eb65
Sourasekhar Banerjee <souras@pelle1.uppmax.uu.se>
2026-06-17 11:39:00 +0200

    apptainer added in sbatch template

--- run_sbatch_template.sh
+++ run_sbatch_template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -A uppmax2025-2-413
 #SBATCH --partition=gpu
-#SBATCH --job-name=fl_cifar
-#SBATCH --output=fl_cifar_%j.out
-#SBATCH --error=fl_cifar_%j.err
+#SBATCH --job-name=fl_cifar_vram_flat
+#SBATCH --output=<filename>_%j.out
+#SBATCH --error=<filename>_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
@@ -14,17 +14,17 @@ echo "Job ID: $SLURM_JOB_ID"
 echo "Node: $SLURMD_NODENAME"
 echo "Start: $(date)"
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate <your_env_name>
+WORKDIR=</path/to/your/workdir>
+SIF=$WORKDIR/fl_cifar.sif
 
-cd <your_project_path>
-
-nvidia-smi --query-gpu=memory.used,memory.free,memory.total \
+nvidia-smi --query-gpu=timestamp,index,memory.used,memory.free,memory.total \
            --format=csv -l 5 >> gpu_mem_log_$SLURM_JOB_ID.csv &
 NVPID=$!
 
-python fl_sequential_cifar.py
+apptainer exec --nv \
+    --bind $WORKDIR:/workspace \
+    $SIF python3 /workspace/fl_sequential_cifar_vram_flat.py
 
 kill $NVPID
-
 echo "End: $(date)"
+
